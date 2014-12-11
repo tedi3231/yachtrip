cdd77811b0684876966bc71e9008a6f906549347
tedi3231 <tedi3231@gmail.com>
2014-12-11 10:46:06 +0800

    add package and yatrip

--- models.py
+++ models.py
@@ -12,35 +12,60 @@ class yatrip_length(models.Model):
     name = fields.Selection([('ft','FT'),('m','M')])
     m_length = fields.Float()
     ft_length = fields.Float()
+    yatrip_id = fields.Many2one('yachtrip.yatrip')
     
 
 class yatrip(models.Model):
     _name = 'yachtrip.yatrip'
     
-    name = fields.Char(size=500)
-    country = fields.Many2one("res.country")
+    name = fields.Char(size=500,required=True)
+    country = fields.Many2one("res.country",required=True)
     province = fields.Many2one("res.country.state",domain="[('country_id','=',country)]")
     price = fields.Float()
     currency = fields.Many2one("res.currency")
     yatriptype = fields.Many2one("yachtrip.yatrip.type")
-    guests = fields.Selection([('0-4','04'),('0-6','06'),('6-8','68'),('8-10','810'),('10-12','1012'),('12+','12+')])
-    length = fields.Many2one('yachtrip.yatrip.length')
+    guests = fields.Selection([('04','0-4'),('06','0-6'),('68','6-8'),('810','8-10'),('1012','10-12'),('12+','12+')])
+    #length = fields.One2many('yachtrip.yatrip.length','yatrip_id')
+    length = fields.Float()
     builder = fields.Many2one('res.partner')
     broker = fields.Many2one('res.partner')
     last_refitted = fields.Date()
     max_speed = fields.Char(size=100)
     cruise_speed = fields.Char(size=100)
     description = fields.Html()
+    images = fields.One2many('ir.attachment',compute='_get_images')
 
+    def _get_images(self):
+        for rec in self:
+            attachments = self.env['ir.attachment'].search([('res_model','=','yachtrip.yatrip'),('res_id','=',rec.id)])
+            self.images = attachments
+
+    def attachment_tree_view(self, cr, uid, ids, context):
+        domain = [
+             '&', ('res_model', '=', 'yachtrip.yatrip'), ('res_id', 'in', ids),
+        ]
+        res_id = ids and ids[0] or False
+        return {
+            'name': _('Documents'),
+            'domain': domain,
+            'res_model': 'ir.attachment',
+            'type': 'ir.actions.act_window',
+            'view_id': False,
+            'view_mode': 'tree,form',
+            'view_type': 'form',
+            'limit': 80,
+            'context': "{'default_res_model': '%s','default_res_id': %d}" % (self._name, res_id)
+        }
 
 class package(models.Model):
     _name = 'yachtrip.package'
-    name = fields.Char(size=500)
+    name = fields.Char(size=500,required=True)
     price = fields.Char(size=100)
-    excepts= fields.Char(size=1000)
+    excepts= fields.Char(size=1000,required=True)
     content = fields.Html()
     special_benefit = fields.Boolean()
-    create_date = fields.Datetime()
+    active = fields.Boolean()
+    add_date = fields.Datetime()
 
     #images = fields.Many2one('ir.attachment',domain="[('res_model','=','yachtrip.package')]")
     
